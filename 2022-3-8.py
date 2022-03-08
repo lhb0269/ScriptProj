51af436b9b6762a7e6d3cb85d70186aeea09999d
lhb0269 <lhb0269@gmail.com>
2022-03-08 11:48:35 +0900

    2022-3-8 화요일 수업

--- 2022-3-8.py
+++ 2022-3-8.py
@@ -1,3 +1,17 @@
-a = 10
-b = 4
-a+b
+import random
+# while True:
+#     print('your name')
+#     name = input()
+#     if 'HBLee' == name:
+#         break
+# print('thank you')
+
+for i in range(0,1000):
+    if i%3 ==0:
+        print(i)
+
+num = 0
+while num<1001:
+    if num%3==0:
+        print(num)
+    num+=1
