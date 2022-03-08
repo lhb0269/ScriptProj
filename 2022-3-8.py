ff7853dbaa638e35345c1a7f7891ea34f56b7b32
lhb0269 <lhb0269@gmail.com>
2022-03-08 13:34:34 +0900

    Update 2022-3-8.py

--- 2022-3-8.py
+++ 2022-3-8.py
@@ -6,12 +6,12 @@ import random
 #         break
 # print('thank you')
 
-for i in range(0,1000):
+for i in range(0,1001):
     if i%3 ==0:
         print(i)
 
 num = 0
-while num<1001:
+while num<=1000:
     if num%3==0:
         print(num)
     num+=1
