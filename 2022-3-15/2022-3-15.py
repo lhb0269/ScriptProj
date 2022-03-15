58658ee5b517cd848dd5dfbc9e7a730f6554b298
lhb0269 <lhb0269@gmail.com>
2022-03-15 12:05:13 +0900

    2022-3-15 수업

---
+++ 2022-3-15/2022-3-15.py
@@ -0,0 +1,48 @@
+import math
+# population = 51_628_117
+# message = '아기 많이 낳읍시다.'
+#
+# print('인구는',population,'명입니다.','원주율은',math.pi,'입니다.',message)
+# print('인구는 %d 명입니다. 원주율은 %.6f 입니다. %s'%(population,math.pi,message))
+#
+# print(f"인구는 {population} 명입니다. 원주율은{math.pi} 입니다.{message}")
+# print(f"인구는 {population:,} 명입니다. 원주율은 {math.pi:.6f} 입니다. {message}")
+#
+# print(f"브레이스는 {{}} 이렇게 씁니다.")
+
+# f= lambda x,y:x+y
+# print(f(10,20))
+#
+# f= lambda x,y,z : x+y+z
+# print(f(1,2,3))
+#
+data = range(0,100)
+#
+# f= lambda x:x**2
+# map(f,data)
+#
+# print(list(map(f,data)))
+#
+# print(list(map(f,map(f,data))))
+
+
+# f= lambda x:x%2==0
+# print(list(filter(f,data)))
+
+def merge(*str):
+    result =''
+    num = 0
+    num2 = 0
+    for s in str:
+        num+=1
+    for s in str:
+        num2+=1
+        if num2 >= num:
+            result+='and '+s
+        else:
+            result += s +','
+    return result
+
+final = merge('orange','apple','mango','banana','peanut')
+print(final)
+print(type(final))
