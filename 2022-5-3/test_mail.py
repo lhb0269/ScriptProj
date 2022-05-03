18b4e7e13f7ca88f6417f1eba03d2a9747543fc4
lhb0269 <lhb0269@gmail.com>
2022-05-03 12:33:50 +0900

    Update test_mail.py

--- 2022-5-3/test_mail.py
+++ 2022-5-3/test_mail.py
@@ -1,6 +1,14 @@
 import smtplib
 from email.message import  EmailMessage
-mail_server = smtplib.SMTP('localhost:8025')
+
+id = 'pythontestlhb@gmail.com'
+passwd = 'gksqlc9784'
+
+
+mail_server = smtplib.SMTP('smtp.gmail.com',587)
+mail_server.ehlo()
+mail_server.starttls()
+mail_server.login(id,passwd)
 #
 # sender = 'lhb0269@naver.com'
 # receiver = 'lhb0269@tukorea.ac.kr'
@@ -9,16 +17,16 @@ mail_server = smtplib.SMTP('localhost:8025')
 # Subject: Test Mail
 # this is auto mail by python
 # """
-id = 'pythontestlhb@gmail.com'
-passwd = 'gksqlc9784'
-msg = EmailMessage()
-msg['Subject'] = 'Test Mail'
-msg['From'] = 'lhb0269@naver.com'
-msg['To'] = 'lhb0269@tukorea.ac.kr'
-msg.set_content('이것은 파이썬으로 자동 발송된 메일입니다.')
 
-with open('Lukaku.jpg','rb')as f:
-    msg.add_attachment(f.read(),maintype ='image',subtype = 'jpeg',filename = 'Lukaku.jpg')
+##교수님께 보내는 메일
+msg = EmailMessage()
+msg['Subject'] = '이한빛 등록 요청'
+msg['From'] = 'pythontestlhb@gmail.com'
+msg['To'] = 'daehyun.lee.python.test@gmail.com'
+msg.set_content('안녕하세요 등록을 요청합니다.')
+
+# with open('Lukaku.jpg','rb')as f:
+#     msg.add_attachment(f.read(),maintype ='image',subtype = 'jpeg',filename = 'Lukaku.jpg')
 
 mail_server.send_message(msg)
 mail_server.quit()
