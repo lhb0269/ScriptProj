be9e53b3957c23bbb518b395be612dc9f6e578f0
lhb0269 <lhb0269@gmail.com>
2022-05-11 12:35:16 +0900

    5-11 수업

--- 2022-5-10/mytelegram.py
+++ 2022-5-10/mytelegram.py
@@ -1,7 +1,7 @@
 import requests
+import time
 
-
-token = '5336659329:AAF-94FBfKrLZUxVjVSLZoIu0lMfWHr4hiY'
+token = '5197089316:AAFsvX63Cs4Tw60IZK8Be1lVK7bym6sdi4I'
 r = requests.get(f'https://api.telegram.org/bot{token}/getMe')
 
 r.headers
@@ -23,52 +23,78 @@ def download_file(file_id,file_name):
         if r.ok:
             wf.write(r.content)
         print('....done')
-
+number = 0
 def print_msg(msg):
     global sender_id
+    global number
     sender_id = msg['from']['id']
     sender = msg['from']['first_name']
     if 'text' in msg:
         text = msg['text']
         print(f'{sender}({sender_id}): {text}')
-    elif 'document' in msg:
+        number = eval(text)
+        print(number)
+    elif 'photo' in msg:
         caption = msg['caption']
-        file_name = msg['document']['file_name']
+        file_name = msg['photo']['file_name']
         print(f'{sender}({sender_id}) : {caption} file name({file_name})')
-
-        file_id = msg['document']['file_id']
-        file_unique_id = msg['document']['file_unique_id']
+        file_id = msg['photo']['file_id']
+        file_unique_id = msg['photo']['file_unique_id']
 
         download_file(file_id,file_name)
 # get message to bot
-
-sender_id = None # me
-url = f'https://api.telegram.org/bot{token}/getUpdates'
-r = requests.get(url)
-r.json()
-if r.ok:
-    update_list = r.json()['result']
-    for update in update_list:
-        print_msg(update['message'])
-
-# # send message to me
-# url = f'https://api.telegram.org/bot{token}/sendMessage'
+while(True):
+    sender_id = None # me
+    url = f'https://api.telegram.org/bot{token}/getUpdates'
+    r = requests.get(url)
+    r.json()
+    alist=[]
+    if len(r.json()['result']):
+        update_list = r.json()['result']
+        for update in update_list:
+            alist.append(update)
+            print_msg(update['message'])
+        last_update_id = alist[-1]['update_id']
+        params = {
+            'offset': last_update_id + 1
+        }
+        r = requests.get(url, params=params)
+        r.json()
+        if r.ok:
+            update_list = r.json()['result']
+            for update in update_list:
+                print_msg(update['message'])
+        # send message to me
+        url = f'https://api.telegram.org/bot{token}/sendMessage'
+        params ={
+            'chat_id': sender_id,
+            'text':number
+        }
+        r = requests.get(url,params=params)
+        time.sleep(2)
+#
+# url =f'https://api.telegram.org/bot{token}/sendDocument'
+# fname = 'asd.jpg'
+# with open(fname,'rb') as df:
+#     params={
+#         'chat_id':sender_id,
+#         'caption':'자료',
+#     }
+#     files={
+#         'document':df
+#     }
+#     r = requests.get(url,params=params,files=files)
+#     print(r)
+# 파일읽기
+# url = f'https://api.telegram.org/bot{token}/getUpdates'
 # params ={
-#     'chat_id': sender_id,
-#     'text':'안녕하세요. 저는 손흥민 봇입니다. 반갑습니다.'
+#     'offset' : 357419078
 # }
 # r = requests.get(url,params=params)
-#
-url =f'https://api.telegram.org/bot{token}/sendDocument'
-fname = 'asd.jpg'
-with open(fname,'rb') as df:
-    params={
-        'chat_id':sender_id,
-        'caption':'자료',
-        'photo':'https://ksc.tukorea.ac.kr/sso/images/login/logo_new.png'
-    }
-    files={
-        'document':df
-    }
-    r=requests.get(url,params=params,files=files)
-    print(r)
+# r.json()
+# 
+# params = {
+#     'file_id':'BQACAgUAAxkBAAMHYnsH66SGqdYIyIetCG_T87QaVSEAAvAEAAIuWdlXEiEkUu9BDjAkBA'
+# }
+# url = f'https://api.telegram.org/bot{token}/getFile'
+# r = requests.get(url,params=params)
